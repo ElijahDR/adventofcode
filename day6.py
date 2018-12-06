68a7ce166dd09b5233b2d349263b3e2daf3a2e31
ElijahDR <musicmayhemandmoney@gmail.com>
2018-12-06 10:34:59 +0000

    Minor changes to day 6

--- day6.py
+++ day6.py
@@ -1,5 +1,7 @@
 import time
 
+arrSize = 500
+
 def cleanLine(line):
     line = line.rstrip()
     line = line.split(", ")
@@ -19,6 +21,22 @@ def emptyArr(x, y, z = 0):
 
     return arr
 
+def getExtremes(data):
+    extremes = [arrSize ** 2, arrSize ** 2, 0, 0]
+    for line in data:
+        line = cleanLine(line)
+        if line[0] < extremes[0]:
+            extremes[0] = line[0]
+        if line[1] < extremes[1]:
+            extremes[1] = line[1]
+        if line[0] > extremes[2]:
+            extremes[2] = line[0]
+        if line[1] > extremes[3]:
+            extremes[3] = line[1]
+
+    return extremes
+
+# Credit goes to the internet
 def manhattan_distance(p, q):
 
     if(len(p) != len(q)):
@@ -28,15 +46,16 @@ def manhattan_distance(p, q):
     return sum([abs(p[i]-q[i]) for i in range(len(p))])
 
 def part1():
+    startTime = time.time()
     with open("input6.txt") as f:
         data = f.readlines()
 
-    arrSize = 500
     extremes = [arrSize ** 2, arrSize ** 2, 0, 0]
     arr = emptyArr(arrSize, arrSize, arrSize ** 2)
     lineArrs = []
     all = []
     z = 0
+
     bestDists = emptyArr(arrSize, arrSize, arrSize**2)
     for line in data:
         line = cleanLine(line)
@@ -76,9 +95,6 @@ def part1():
         if arr[i][arrSize - 1] in notInfinite:
             notInfinite.remove(arr[i][arrSize - 1])
 
-
-
-
     bestCount = 0
     bestI = 0
     for i in notInfinite:
@@ -92,22 +108,26 @@ def part1():
             bestCount = count
             bestI = i
 
+    print("Running took %s seconds" % (time.time() - startTime))
     print("day 6, part 1:", bestCount)
 
     # print(arr)
 
-def part2():
+def part2(extremes = 0):
+    startTime = time.time()
     with open("input6.txt") as f:
         data = f.readlines()
-    arrSize = 500
+
+    if extremes == 0:
+        extremes = getExtremes(data)
     lines = []
     for line in data:
         line = cleanLine(line)
         lines.append(line)
     count  = 0
     arr = emptyArr(arrSize, arrSize, arrSize**2)
-    for i in range(arrSize):
-        for j in range(arrSize):
+    for i in range(extremes[0], extremes[2]):
+        for j in range(extremes[1], extremes[3]):
             total = 0
             for line in lines:
                 total += manhattan_distance([i, j], line)
@@ -115,6 +135,7 @@ def part2():
             if total < 10000:
                 count += 1
 
+    print("Running took %s seconds" % (time.time() - startTime))
     print("day 6, part 2:", count)
 
 def main():
